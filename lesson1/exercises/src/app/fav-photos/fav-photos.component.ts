b28fef99fd91ef274c5686b42b296e5a0053e561
saranell <76405224+saranell@users.noreply.github.com>
2022-07-20 13:39:04 -0500

    exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'fav-photos',
-  templateUrl: './fav-photos.component.html',
-  styleUrls: ['./fav-photos.component.css']
+  selector: "fav-photos",
+  templateUrl: "./fav-photos.component.html",
+  styleUrls: ["./fav-photos.component.css"],
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "Frogs Make Me Happy";
+  image1 =
+    "https://i.pinimg.com/736x/46/04/0a/46040a61360717868e5262a8eb0ef075.jpg";
+  image2 =
+    "https://i.pinimg.com/564x/56/2f/a7/562fa755855d9846afa1179b346b70a9.jpg";
+  image3 =
+    "https://i.pinimg.com/564x/1d/c8/2d/1dc82df5536334b735ad551f5e894303.jpg";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
