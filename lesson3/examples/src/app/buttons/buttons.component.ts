3e10cd7d812169158070a0c39378996153accb92
saranell <76405224+saranell@users.noreply.github.com>
2022-07-27 13:14:16 -0500

    Finished examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'buttons',
-   templateUrl: './buttons.component.html',
-   styleUrls: ['./buttons.component.css']
+  selector: "buttons",
+  templateUrl: "./buttons.component.html",
+  styleUrls: ["./buttons.component.css"],
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+  buttonHeading: string = "Buttons";
+  active: boolean = true;
+  buttonState: boolean[] = [true, true, true];
+  location: string = "center";
+  constructor() {}
+  ngOnInit() {}
 
-   constructor() { }
+  reactivateButtons() {
+    for (let i = 0; i < this.buttonState.length; i++) {
+      this.buttonState[i] = true;
+    }
+    return this.buttonState;
+  }
 
-   ngOnInit() { }
-
+  //   joke button
+  shiftLocation(oldLocation: string) {
+    while (this.location === oldLocation) {
+      this.location = ["left", "right", "center"][
+        Math.floor(Math.random() * 3)
+      ];
+    }
+    return this.location;
+  }
 }
